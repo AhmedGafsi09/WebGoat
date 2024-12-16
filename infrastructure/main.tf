aed8ad0984da73b262312fd0cb7daee0abfafe37
Ahmed Gafsi <ahmed.gafsi@polymtl.ca>
2024-12-16 17:17:35 -0500

    Deploy with ansible

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,135 +1,85 @@
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-}
-
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
-
-# Variables
-variable "ssh_public_key" {
-  description = "Public SSH key for VM access"
-  type        = string
-}
-
-# Resource Group
-resource "azurerm_resource_group" "webgoat_rg" {
-  name     = "webgoat-rg"
-  location = "East US"
-}
-
-# Network Security Group
-resource "azurerm_network_security_group" "webgoat_nsg" {
-  name                = "webgoat-nsg"
-  location            = azurerm_resource_group.webgoat_rg.location
-  resource_group_name = azurerm_resource_group.webgoat_rg.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range         = "*"
-    destination_port_range    = "22"
-    source_address_prefix     = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "WebGoat"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range         = "*"
-    destination_port_range    = "8080"
-    source_address_prefix     = "*"
-    destination_address_prefix = "*"
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.23.0"
+    }
   }
 }
 
-# Virtual Network
-resource "azurerm_virtual_network" "webgoat_vnet" {
-  name                = "webgoat-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.webgoat_rg.location
-  resource_group_name = azurerm_resource_group.webgoat_rg.name
+provider "kubernetes" {
+  config_path = "~/.kube/config"
 }
 
-# Subnet
-resource "azurerm_subnet" "webgoat_subnet" {
-  name                 = "webgoat-subnet"
-  resource_group_name  = azurerm_resource_group.webgoat_rg.name
-  virtual_network_name = azurerm_virtual_network.webgoat_vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# Public IP
-resource "azurerm_public_ip" "webgoat_pip" {
-  name                = "webgoat-pip"
-  location            = azurerm_resource_group.webgoat_rg.location
-  resource_group_name = azurerm_resource_group.webgoat_rg.name
-  allocation_method   = "Static"
-}
-
-# Network Interface
-resource "azurerm_network_interface" "webgoat_nic" {
-  name                = "webgoat-nic"
-  location            = azurerm_resource_group.webgoat_rg.location
-  resource_group_name = azurerm_resource_group.webgoat_rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.webgoat_subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.webgoat_pip.id
+# Create Namespaces
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
   }
 }
 
-# VM
-resource "azurerm_linux_virtual_machine" "webgoat_vm" {
-  name                = "webgoat-vm"
-  location            = azurerm_resource_group.webgoat_rg.location
-  resource_group_name = azurerm_resource_group.webgoat_rg.name
-  size                = "Standard_B2s"
-  admin_username      = "azureuser"
-
-  network_interface_ids = [
-    azurerm_network_interface.webgoat_nic.id
-  ]
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = var.ssh_public_key
+resource "kubernetes_namespace" "webgoat" {
+  metadata {
+    name = "webgoat"
   }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
-  custom_data = base64encode(<<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y docker.io docker-compose git
-              systemctl enable docker
-              systemctl start docker
-              usermod -aG docker azureuser
-              EOF
-  )
 }
 
-# Outputs
-output "public_ip" {
-  value = azurerm_public_ip.webgoat_pip.ip_address
+# Deploy Prometheus
+resource "kubernetes_deployment" "prometheus" {
+  metadata {
+    name = "prometheus"
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "prometheus"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "prometheus"
+        }
+      }
+      spec {
+        container {
+          name = "prometheus"
+          image = "prom/prometheus"
+          port {
+            container_port = 9090
+          }
+          resources {
+            limits = {
+              cpu    = "500m"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "256Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+# Service for Prometheus
+resource "kubernetes_service" "prometheus" {
+  metadata {
+    name = "prometheus"
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+  }
+  spec {
+    selector = {
+      app = "prometheus"
+    }
+    port {
+      port = 9090
+      target_port = 9090
+    }
+    type = "NodePort"
+  }
 }
